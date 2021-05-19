4cd9be52cd48ba9cc359e3ba99977505b6c24976
Mridul2820 <immridulpanda2820@gmail.com>
2021-05-20 01:00:56 +0530

    slider extend

--- src/components/HomeMain.js
+++ src/components/HomeMain.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 
 // Social
 import facebookImg from "../assets/social/facebook.svg";
@@ -15,8 +14,9 @@ import "slick-carousel/slick/slick-theme.css";
 import slide1 from "../assets/slide/airbnb-clone.png";
 import slide2 from "../assets/slide/disney-plus-clone.png";
 import slide3 from "../assets/slide/netflix-build.png";
-
-
+import slide4 from "../assets/slide/music-player.png";
+import slide5 from "../assets/slide/restaurant-site.png";
+import slide6 from "../assets/slide/google-clone.png";
 
 const HomeMain = () => {
 
@@ -83,19 +83,34 @@ const HomeMain = () => {
             <div className="homemain__right">
                 <Slider {...settings} className="homemain__slider">
                     <div className="homemain__slide">
-                        <Link to="https://airbnb-clone-sigma.vercel.app/">
-                            <img src={slide1} alt="" />
-                        </Link>
+                        <a href="https://airbnb-clone-sigma.vercel.app/">
+                            <img src={slide1} alt="Airbnb Clone" />
+                        </a>
                     </div>
                     <div className="homemain__slide">
-                        <Link to="https://disney-plus-clone-gilt.vercel.app/">
-                            <img src={slide2} alt="" />
-                        </Link>
+                        <a href="https://disney-plus-clone-gilt.vercel.app/">
+                            <img src={slide2} alt="Disney Plus Clone" />
+                        </a>
                     </div>
                     <div className="homemain__slide">
-                        <Link to="https://netflix-build-beta.vercel.app/">
-                            <img src={slide3} alt="" />
-                        </Link>
+                        <a href="https://netflix-build-beta.vercel.app/">
+                            <img src={slide3} alt="Netflix Clone" />
+                        </a>
+                    </div>
+                    <div className="homemain__slide">
+                        <a href="https://music-player-mridul28.vercel.app/">
+                            <img src={slide4} alt="Music Player" />
+                        </a>
+                    </div>
+                    <div className="homemain__slide">
+                        <a href="https://restaurant-site.vercel.app/">
+                            <img src={slide5} alt="Restaurant" />
+                        </a>
+                    </div>
+                    <div className="homemain__slide">
+                        <a href="https://google-clone-blond-one.vercel.app/">
+                            <img src={slide6} alt="Google Clone" />
+                        </a>
                     </div>
                 </Slider>
             </div>
