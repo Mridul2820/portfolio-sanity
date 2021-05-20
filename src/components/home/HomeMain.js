5099f7a0d5a37720dcc4641ca7ff7710229eb790
Mridul2820 <immridulpanda2820@gmail.com>
2021-05-20 12:17:35 +0530

    home buttons

--- src/components/home/HomeMain.js
+++ src/components/home/HomeMain.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import resume from '../../assets/MRIDUL.pdf';
 
 import SocialLinks from '../SocialLinks';
 import HomeSlider from './HomeSlider';
@@ -13,10 +14,22 @@ const HomeMain = () => {
                     <h3>A Passionate Web Developer </h3>
                     <p>building Websites with HTML, CSS, JavaScript & REACT and some cool libraries and frameworks.</p>
                 </div>
+                <div className="homemain__buttons">
+                    <a href="mailto:mridulpanda2820@gmail.com" className="homemain__button">Get In Touch</a>
+                    <a 
+                        target="_blank" 
+                        rel="noreferrer"
+                        href={resume} 
+                        className="homemain__button"
+                    >
+                        See My Resume
+                    </a>
+                </div>
                 <SocialLinks />
             </div>
             <div className="homemain__right">
                 <HomeSlider />
+                <h3>Some of My recent Projects</h3>
             </div>
         </div>
     )
