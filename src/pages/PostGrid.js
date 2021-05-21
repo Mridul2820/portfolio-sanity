1bf057f4a1f369f743af06d23cbfdb7843b6fed4
Mridul2820 <immridulpanda2820@gmail.com>
2021-05-21 19:44:59 +0530

    postgrid to styled components

--- src/pages/PostGrid.js
+++ src/pages/PostGrid.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
 import sanityClient from '../client.js'
 import Post from '../components/postgrid/Post'
 
@@ -25,23 +26,78 @@ const PostGrid = () => {
     }, [])
 
     return (
-        <main className="posts">
-            <section className="posts__section">
-                <div className="posts__header">
+        <StyledMain>
+            <section>
+                <StyledHeader>
                     <h1>Blog Posts</h1>
                     <p>Welcome to my blog posts page!</p>
-                </div>
+                </StyledHeader>
                 
-                <div className="posts__grid">
+                <StyledGrid>
                 {postData &&
                     postData.map((post) => (
                         <Post key={post.slug.current} post={post}/>
                     ))
                 }
-                </div>
+                </StyledGrid>
             </section>
-        </main>
+        </StyledMain>
     )
 }
+
+const StyledMain = styled.main`
+    min-height: 85vh;
+    background-color: #f0fff4;
+
+    section {
+        width: 90vw;
+        margin: 0 auto;
+
+        @media (max-width: 840px) {
+            width: 95vw;
+        }
+    }
+`
+
+const StyledHeader = styled.main`
+    text-align: center;
+
+    h1 {
+        padding-top: 20px;
+        font-size: 3rem;
+
+        @media (max-width: 500px) {
+            font-size: 2rem;
+        }
+    }
+
+    p {
+        font-size: 1.5rem;
+    }
+`
+
+const StyledGrid = styled.main`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 50px;
+    justify-content: center;
+    margin-top: 50px;
+    padding-bottom: 30px;
+
+    @media (max-width: 992px) {
+        gap: 20px;
+    }
+
+    @media (max-width: 840px) {
+        grid-template-columns: repeat(2, 1fr);
+    }
+
+    @media (max-width: 500px) {
+        grid-template-columns: repeat(1, 1fr);
+        gap: 40px;
+        margin-top: 20px;
+    }
+`
+
 
 export default PostGrid
