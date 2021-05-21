4151ddd859d05733fb4d7de4da1dbaa7ecdcdd74
Mridul2820 <immridulpanda2820@gmail.com>
2021-05-21 19:40:28 +0530

    home contact to styled component

--- src/components/home/HomeContact.js
+++ src/components/home/HomeContact.js
@@ -1,23 +1,91 @@
 import React from 'react'
+import styled from 'styled-components';
 import SocialLinks from '../SocialLinks'
 import contact from '../../assets/contact.svg'
 
 const HomeContact = () => {
     return (
-        <div className="homecontact">
-            <div className="homecontact__left">
-                <div className="homecontact__text">
+        <StyledContainer>
+            <StyledLeft>
+                <StyledText>
                     <h2>Contact Me ☎️</h2>
                     <p>DISCUSS A PROJECT OR JUST WANT TO SAY HI? MY INBOX IS OPEN FOR ALL.</p>
                     <h3>Main : contact@mridul.tech</h3>
-                </div>
+                </StyledText>
                 <SocialLinks />
-            </div>
-            <div className="homecontact__right">
+            </StyledLeft>
+            <StyledRight>
                 <img src={contact} alt="contact" />
-            </div>
-        </div>
+            </StyledRight>
+        </StyledContainer>
     )
 }
+
+const StyledContainer = styled.div`
+    display: flex;
+    align-items: center;
+    width: 90vw;
+    margin: 50px auto;
+    padding-top: 50px;
+    height: 100%;
+    border-top: 3px solid #96e6a1 ;
+
+    @media (max-width: 992px) {
+        width: 95vw;
+    }
+
+    @media (max-width: 840px) {
+        flex-direction: column;
+        min-height: 100%;
+    }
+`
+
+const StyledLeft = styled.div`
+    width: 55%;
+
+    @media (max-width: 840px) {
+        width: 80%;
+        margin: 40px;
+    }
+
+    @media (max-width: 500px) {
+        width: 100%;
+        padding: 0 10px;
+        text-align: center;
+
+        .social {
+            justify-content: center;
+        }
+    }
+`
+
+const StyledRight = styled.div`
+    width: 45%;
+
+    @media (max-width: 840px) {
+        width: 80%;
+        margin-bottom: 60px;
+    }
+
+    @media (max-width: 500px) {
+        width: 100%;
+        padding: 0 10px;
+    }
+
+    img {
+        width: 100%;
+    }
+`
+const StyledText = styled.div`
+    h2 {
+        font-size: 2.5rem;
+        margin-bottom: 20px;
+    }
+
+    h3 {
+        margin-top: 10px;
+    }
+`
+
 
 export default HomeContact
