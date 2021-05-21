43b495421f018f681e4d01d372f6f458fa2d991f
Mridul2820 <immridulpanda2820@gmail.com>
2021-05-21 19:48:35 +0530

    blog posts to styled components

--- src/components/postgrid/Post.js
+++ src/components/postgrid/Post.js
@@ -1,30 +1,61 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 const Post = ({ post }) => {
     return (
-        <article className="post" >
+        <StyledPost>
             <Link to={"/post/" + post.slug.current}  >
-                <div className="post__image">
+                <StyledImg>
                     <img
                         src={post.mainImage.asset.url}
                         alt={post.mainImage.alt}
                     />
-                </div>
-                <div className="post__text">
-                    <h3 className="post__title">
+                </StyledImg>
+                <StyledText>
+                    <h3>
                         {post.title}
                     </h3>
-                    <span className="post__date">
+                    <span>
                         {new Date(post.publishedAt).toDateString()}
                     </span>
                     {/* <p className="post__desc">
                         {post.body}
                     </p> */}
-                </div>
+                </StyledText>
             </Link>
-        </article>
+        </StyledPost>
     )
 }
+
+const StyledPost = styled.article`
+    width: 100%;
+
+    a {
+        width: 100%;
+    }
+`
+
+const StyledImg = styled.div`
+    width: 100%;
+    border-radius: 3px;
+    overflow: hidden;
+
+    img {
+        width: 100%;
+        object-fit: cover;
+        vertical-align: bottom;
+    }
+`
+const StyledText = styled.div`
+    margin-top: 10px;
+    padding-left: 10px;
+    border-left: 3px solid #96e6a1;
+
+    h3 {
+        font-size: 22px;
+    }
+`
+
 
 export default Post
