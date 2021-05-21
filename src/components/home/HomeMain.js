be2c7cf4fadb24ef8a7dbc349cb1067b9573bba7
Mridul2820 <immridulpanda2820@gmail.com>
2021-05-21 16:17:10 +0530

    home main converted to styled component

--- src/components/home/HomeMain.js
+++ src/components/home/HomeMain.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import styled from 'styled-components'
 
 import resume from '../../assets/MRIDUL.pdf';
 
@@ -7,32 +7,139 @@ import HomeSlider from './HomeSlider';
 
 const HomeMain = () => {
     return (
-        <main className="homemain">
-            <div className="homemain__left">
-                <div className="homemain__textbox">
+        <StyledContainer>
+            <StyledLeft>
+                <StyledTextbox>
                     <h2>Hi, I'm Mridul</h2>
                     <h3>A Passionate Web Developer </h3>
                     <p>building Websites with HTML, CSS, JavaScript & REACT and some cool libraries and frameworks.</p>
-                </div>
-                <div className="homemain__buttons">
-                    <a href="mailto:mridulpanda2820@gmail.com" className="homemain__button">Get In Touch</a>
+                </StyledTextbox>
+                <StyledButtons>
+                    <a href="mailto:mridulpanda2820@gmail.com" className="main-button">Get In Touch</a>
                     <a 
                         target="_blank" 
                         rel="noreferrer"
                         href={resume} 
-                        className="homemain__button"
+                        className="main-button"
                     >
                         See My Resume
                     </a>
-                </div>
+                </StyledButtons>
                 <SocialLinks />
-            </div>
-            <div className="homemain__right">
+            </StyledLeft>
+            <StyledRight>
                 <HomeSlider />
                 <h3>Some of My recent Projects</h3>
-            </div>
-        </main>
+            </StyledRight>
+        </StyledContainer>
     )
 }
+
+const StyledContainer = styled.main`
+    display: flex;
+    align-items: center;
+    width: 90vw;
+    margin: 0 auto;
+    height: 100%;
+    min-height: 90vh;
+
+    @media (max-width: 992px) {
+        width: 95vw;
+    }
+
+    @media (max-width: 840px) {
+        flex-direction: column;
+        min-height: 100%;
+    }
+`
+
+const StyledLeft = styled.div`
+    width: 45%;
+
+    @media (max-width: 840px) {
+        width: 80%;
+        margin: 40px;
+    }
+
+    @media (max-width: 500px) {
+        width: 100%;
+        padding: 0 10px;
+    }
+`
+
+const StyledTextbox = styled.div`
+    max-width: 90%;
+
+    @media (max-width: 500px) {
+        max-width: 100%;
+    }
+
+    h2 {
+        font-size: 3rem;
+
+        @media (max-width: 500px) {
+            font-size: 2.5rem;
+        }
+    }
+
+    h3 {
+        font-size: 1.5rem;
+        margin-bottom: 10px;
+
+        @media (max-width: 500px) {
+            font-size: 1.2rem;
+        }
+    }
+
+    p {
+        font-size: 1.2rem;
+
+        @media (max-width: 500px) {
+            font-size: 1rem;
+        }
+    }
+`
+
+const StyledButtons = styled.div`
+    margin-top: 30px;
+    display: flex;
+
+    .main-button{
+        margin-right: 20px;
+        padding: 8px 16px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 1.2rem;
+        font-family:'Lato';
+        border: 2px solid #023047;
+        background-color: #023047;
+        box-shadow: 3px 6px 20px rgba(64, 149, 198, 0.336);
+        border-radius: 5px;
+        color:#ffffff;
+    
+        &:hover{
+            background-color: transparent;
+            transition: all ease 0.5s;
+            color: #3d2514;
+        }
+    }
+`
+
+
+const StyledRight = styled.div`
+    width: 55%;
+    text-align: center;
+
+    @media (max-width: 840px) {
+        width: 80%;
+    }
+
+    @media (max-width: 500px) {
+        width: 100%;
+        padding: 0 10px;
+    }
+`
+
 
 export default HomeMain
