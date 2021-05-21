9b1d53ccb2658faa62bf5b69cc4864c52ee09ea4
Mridul2820 <immridulpanda2820@gmail.com>
2021-05-21 16:03:25 +0530

    convert navbar to styled component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import styled from 'styled-components'
 import { NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
 
@@ -18,9 +19,9 @@ const NavBar = () => {
     window.addEventListener('scroll', changeBackground)
 
     return (
-        <div className={nav ? 'navbar active' : 'navbar'}>
-            <nav className="navbar__nav">
-                <div className="navbar__logo">
+        <StyledNavbar className={nav ? 'navbar active' : 'navbar'}>
+            <StyledNav>
+                <StyledLogo>
                     <NavLink 
                         to="/" 
                         className="logo" 
@@ -29,33 +30,116 @@ const NavBar = () => {
                         <img src={logo} alt="logo mridul"/>
                         <h2>ridul</h2>
                     </NavLink>
-                </div>
-                <div className="navbar__links">
+                </StyledLogo>
+                <StyledNavLinks>
                     <NavLink 
                         to="/projects" 
-                        className="navbar__link"
-                        activeClassName="navbar__linkActive"
+                        className="navlink"
+                        activeClassName="navlink-active"
                     >
                         Projects
                     </NavLink>
                     <NavLink 
                         to="/blog"
-                        className="navbar__link"
-                        activeClassName="navbar__linkActive"
+                        className="navlink"
+                        activeClassName="navlink-active"
                     >
                         Blog
                     </NavLink>
                     <NavLink 
                         to="/about"
-                        className="navbar__link"
-                        activeClassName="navbar__linkActive"
+                        className="navlink"
+                        activeClassName="navlink-active"
                     >
                         About Me!
                     </NavLink>
-                </div>
-            </nav>
-        </div>
+                </StyledNavLinks>
+            </StyledNav>
+        </StyledNavbar>
     )
 }
+
+
+const StyledNavbar = styled.div`
+    position: sticky;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 3.5rem;
+    background-image: linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%);
+    background-color: #ffffff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 100;
+    transition: 0.6s ease-in-out;
+
+    &.active{
+        box-shadow: 5px 10px 30px rgba(0, 0, 0, 0.157);
+    }
+`
+
+const StyledNav = styled.nav`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 100%;
+    width: 90vw;
+    max-width: 1170px;
+
+    @media (max-width: 500px) {
+        width: 95vw;
+    }
+`
+
+const StyledLogo = styled.div`
+    .logo {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    
+        img {
+            width: 26px;
+        }
+    
+        h2 {
+            font-size: 1.2rem;
+            font-weight: bold;
+        }
+    }
+`
+
+const StyledNavLinks = styled.div`
+    .navlink {
+        font-weight: 600;
+        margin: 0 5px;
+        padding: 8px 14px;
+        border-radius: 3px;
+        text-transform: uppercase;
+        font-family: "Bebas Neue";
+        letter-spacing: 1px;
+        font-size: 1.2rem;
+        text-transform: uppercase;
+        font-weight: 500;
+        transition: 0.2s ease-in-out;
+
+        @media (max-width: 500px) {
+            padding: 4px 8px;
+        }
+
+        &:hover {
+            background-color: #023047;
+            color: #fff;
+        }
+    }
+
+    .navlink-active {
+        color: #fff;
+        background-color: #023047;
+        box-shadow: 3px 6px 20px rgba(64, 149, 198, 0.336);
+    }
+`
+
+
 
 export default NavBar
